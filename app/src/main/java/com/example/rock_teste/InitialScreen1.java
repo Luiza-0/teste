e9d6b5d5a8ce406f26498e1d7c7593ed04a182df
LAMI 1 <lami1@ucsal.br>
2023-09-23 12:59:32 -0300

    first commit

---
+++ app/src/main/java/com/example/rock_teste/InitialScreen1.java
@@ -0,0 +1,14 @@
+package com.example.rock_teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitialScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initial_screen);
+    }
+}
